8778283e70607d5489f97eea49acfeb825c82f60
Michael Vieck <vieck@purdue.edu>
2019-08-18 20:23:50 -0400

    Fixed fragment navigation.

--- home/src/main/java/com/example/home/ui/fragments/HomeFragment.kt
+++ home/src/main/java/com/example/home/ui/fragments/HomeFragment.kt
@@ -6,14 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-
 import com.example.home.R
 
 class HomeFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
