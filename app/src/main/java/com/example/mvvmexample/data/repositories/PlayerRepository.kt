0aec1141f33e2fe42b4aec389c0c5c1ea9558ba8
Michael Vieck <vieck@purdue.edu>
2019-08-26 20:41:35 -0400

    Hooked up rxandroid and rxjava observables.

--- app/src/main/java/com/example/mvvmexample/data/repositories/PlayerRepository.kt
+++ app/src/main/java/com/example/mvvmexample/data/repositories/PlayerRepository.kt
@@ -1,10 +1,15 @@
 package com.example.mvvmexample.data.repositories
 
 import com.example.mvvmexample.data.services.PlayerService
+import io.reactivex.Flowable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 class PlayerRepository(private val playerService: PlayerService) {
 
-    fun getRecentlyPlayedTracks(limit: Int) {
+    fun getRecentlyPlayedTracks(limit: Int): Flowable<Any> {
         return playerService.getRecentlyPlayedTracks(limit)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
     }
 }
--- app/src/main/java/com/example/mvvmexample/data/services/PlayerService.kt
+++ app/src/main/java/com/example/mvvmexample/data/services/PlayerService.kt
@@ -1,9 +1,10 @@
 package com.example.mvvmexample.data.services
 
+import io.reactivex.Flowable
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface PlayerService {
     @GET("/me/player/recently-played")
-    fun getRecentlyPlayedTracks(@Query("limit") limit: Int)
+    fun getRecentlyPlayedTracks(@Query("limit") limit: Int): Flowable<Any>
 }
