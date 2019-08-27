22bf10bc7060fe490b174d0b783c4fc4c9cfcd03
Michael Vieck <vieck@purdue.edu>
2019-08-26 21:07:21 -0400

    Added gson.

--- app/src/main/java/com/example/mvvmexample/data/models/PlayHistory.kt
+++ app/src/main/java/com/example/mvvmexample/data/models/PlayHistory.kt
@@ -1,9 +1,13 @@
 package com.example.mvvmexample.data.models
 
+import com.google.gson.annotations.SerializedName
 import java.util.*
 
 data class PlayHistory(
+    @SerializedName("track")
     val track: Any,
+    @SerializedName("played_at")
     val playedAt: Date,
+    @SerializedName("context")
     val context: SpotifyContext
 )
--- app/src/main/java/com/example/mvvmexample/data/models/SpotifyContext.kt
+++ app/src/main/java/com/example/mvvmexample/data/models/SpotifyContext.kt
@@ -1,7 +1,12 @@
 package com.example.mvvmexample.data.models
 
+import com.google.gson.annotations.SerializedName
+
 data class SpotifyContext(
+    @SerializedName("type")
     val type: String,
+    @SerializedName("href")
     val href: String,
+    @SerializedName("uri")
     val uri: String
 )
