44ee9a04f3433aa66ddb017fb5230eef999b9009
Michael Vieck <vieck@purdue.edu>
2019-09-01 21:14:21 -0400

    Using list instead of an array.

--- app/src/main/java/com/example/mvvmexample/data/models/CursorPaging.kt
+++ app/src/main/java/com/example/mvvmexample/data/models/CursorPaging.kt
@@ -2,7 +2,7 @@ package com.example.mvvmexample.data.models
 
 data class CursorPaging<T>(
     val href: String,
-    val items: Array<T>,
+    val items: List<T>,
     val limit: Int,
     val next: String,
     val total: Int
