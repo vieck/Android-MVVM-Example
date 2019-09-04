b33b9476f54f75287ce7ab88134a3af6ada49a81
Michael Vieck <vieck@purdue.edu>
2019-09-03 23:24:28 -0400

    Removed over annotating.

--- app/src/main/java/com/example/mvvmexample/data/models/SpotifyContext.kt
+++ app/src/main/java/com/example/mvvmexample/data/models/SpotifyContext.kt
@@ -1,13 +1,9 @@
 package com.example.mvvmexample.data.models
 
 import com.example.mvvmexample.repository.dtos.ISpotifyContext
-import com.squareup.moshi.Json
 
 data class SpotifyContext(
-    @Json(name = "type")
     override val type: String,
-    @Json(name = "href")
     override val href: String,
-    @Json(name = "uri")
     override val uri: String
 ): ISpotifyContext
