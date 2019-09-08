d619509f12dff0eccecbbb46b30c44b8a9a4074d
Michael Vieck <vieck@purdue.edu>
2019-09-07 23:08:39 -0400

    Loading playlist module dynamically from bottom nav.

--- navigation/src/main/java/com/example/mvvmexample/navigation/Destinations.kt
+++ navigation/src/main/java/com/example/mvvmexample/navigation/Destinations.kt
@@ -2,6 +2,7 @@ package com.example.mvvmexample.navigation
 
 enum class Destinations(val intentPath: String) {
     HOME("com.example.mvvmexample.home.ui.fragments.HomeFragment"),
+    PLAYLIST("com.example.mvvmexample.playlists.ui.fragments.PlaylistFragment"),
     NAVIGATION("com.example.mvvmexample.activities.NavigationActivity"),
     MAIN("com.example.mvvmexample.activities.MainActivity")
 }
